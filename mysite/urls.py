c443e9dca76fd1c242218491d791d52c7beb0f7a
leonardo <leonardofais2@gmail.com>
2018-05-15 18:32:41 +0200

    aggiunta funzionalità rimuovi post sicurezza con login e commenti post con gestione

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
@@ -8,4 +10,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
